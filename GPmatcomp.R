ebc4ffae81824d01698860e1cde4cfa464ad8162
Alexander Baker <arbaker@kersel.com>
2015-01-29 14:55:12 +1000

    FIXED IT, separate estimation of mean made it inconsistent

--- GPmatcomp.R
+++ GPmatcomp.R
@@ -298,7 +298,7 @@ PonNconv<-function(p,r,index) {
   return (m)
 }
 
-TWidomTest<-function(index, r,p, k,directory, graph,PaGlist,moment) { ### Issue with Pmax and TWd
+TWidomTest<-function(index, r,p, k,directory, graph,PaGlist) { ### Issue with Pmax and TWd
   q<-vector("list",length(PaGlist[,1]))
   Shift<-vector("list",length(PaGlist[,1]))
   Q<-PonNconv(p,r,index)
@@ -313,14 +313,8 @@ TWidomTest<-function(index, r,p, k,directory, graph,PaGlist,moment) { ### Issue 
     TWd<-0
     l<-q[[i]]$Peig[,1]
     qden<-density(l)
-    if (moment==TRUE) {
     Md[i]<-sum(((qden$x*qden$y))*(qden$x[2]-qden$x[1])) ### first raw moment = E(X)=mean
     TSd[i]<-sqrt(sum(((qden$x-Md[i])^2*qden$y))*(qden$x[2]-qden$x[1])) ### sqrt of the 2nd central moment about the mean
-    }
-    else {
-    Md[i]<-mean(q$Peig[,1])a
-    TSd[i]<-sd(q$Peig[,1])  
-    }
     Pmax<-PaGlist[i,1]
     TWd<-(-1.206+(1.268/TSd[i])*(Pmax-Md[i])) #### used MTW and STW^2 from Saccenti et al
     Shift[[i]]<-(-1.206+(1.268/TSd[i])*(q[[i]]$Peig[,1]-Md[i]))
